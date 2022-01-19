f632b25ed528c04cf3b79fe669bc4fe84febbe76
AdekWHATT <vyacheslaw.kilin@mail.ru>
2022-01-19 20:15:44 +0300

    Delete ...

--- src/index.js
+++ src/index.js
@@ -4,19 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import configureStore from './redux/store'
-import  {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import 'antd/dist/antd.css';
 const store = configureStore()
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
